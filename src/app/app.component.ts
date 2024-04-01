734d0c7cc30bb32a4fb3249462d1d1b749d3f6c6
Anti Nou <anti_nou@hotmail.com>
2024-04-01 14:07:24 +0300

    component input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,20 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 })
 export class AppComponent {
   name = 'Antigoni';
+  
+  person0 = { 
+    givenName: 'Antigoni',
+    surName: 'Nousia',
+    age: 29,
+    email: 'anti_nou@hotmail.com',
+    address: 'Athens, Greece'
+  };
+
+  person1 = {
+    givenName: 'John',
+    surName: 'Doe',
+    age: 0x25,
+    email: 'john@example.com',
+    address: 'New York, USA'
+  };
 }
--- src/app/components/person-table/person-table.component.ts
+++ src/app/components/person-table/person-table.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Person } from 'src/app/shared/interfaces/person';
 
 @Component({
   selector: 'app-person-table',
@@ -8,10 +9,6 @@ import { Component } from '@angular/core';
   styleUrl: './person-table.component.css'
 })
 export class PersonTableComponent {
-  person = {
-    givenName: 'Antigoni',
-    surName: 'Nousia',
-    age: 29,
-    email: 'anti_nou@hotmail.com',
-  };
+  @Input () person: Person | undefined; 
+  
 }
