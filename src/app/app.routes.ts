dfbc1f849b887d445eec49f97d4789012841293a
Anti Nou <anti_nou@hotmail.com>
2024-04-01 22:36:52 +0300

    simple datatable primer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { EventBindExampleComponent } from './components/event-bind-example/event
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { ComponentInputExampleComponent } from './components/component-input-example/component-input-example.component';
 import { ComponentForDirectiveExampleComponent } from './components/component-for-directive-example/component-for-directive-example.component';
+import { SimpleDatatableExampleComponent } from './components/simple-datatable-example/simple-datatable-example.component';
 
 export const routes: Routes = [
     {path: 'for-directive-example', component: ComponentForDirectiveExampleComponent},
     {path: 'component-input-example', component: ComponentInputExampleComponent},
     {path: 'event-bind-example', component: EventBindExampleComponent},
     {path: '', component: WelcomeComponent},
+    {path: 'simple-datatable-example', component: SimpleDatatableExampleComponent}
     
 ];
