946f69f54b7bf35701e86ef6b25a38c3ae38cd22
Anti Nou <anti_nou@hotmail.com>
2024-04-01 13:13:49 +0300

    new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   name = 'Antigoni';
-
-  person ={
-    givenName: 'Antigoni',
-    surName: 'Nousia',
-    age: 29,
-    email: 'anti_nou@hotmail.com',
-  };
 }
