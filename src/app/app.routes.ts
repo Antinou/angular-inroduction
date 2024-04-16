44ecef423d3f19514762c6733e36334b70eeb185
Anti Nou <anti_nou@hotmail.com>
2024-04-16 21:36:17 +0300

    user auth primer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,8 @@ import { TemplateDrivenFormExampleComponent } from './components/template-driven
 import { ReactiveFormExampleComponent } from './components/reactive-form-example/reactive-form-example.component';
 import { HttpClientExampleComponent } from './components/http-client-example/http-client-example.component';
 import { UserRegistrationComponent } from './components/user-registration/user-registration.component';
+import { RestrictedContentExampleComponent } from './components/restricted-content-example/restricted-content-example.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
 
 export const routes: Routes = [
     {path: 'for-directive-example', component: ComponentForDirectiveExampleComponent},
@@ -21,5 +23,6 @@ export const routes: Routes = [
     {path: 'reactive-form-example', component: ReactiveFormExampleComponent},
     {path: 'http-client-example', component: HttpClientExampleComponent},
     {path: 'user-registration-example', component: UserRegistrationComponent},
-    
+    {path: 'restricted-content-example', component: RestrictedContentExampleComponent},
+    {path: 'login', component: UserLoginComponent},
 ];
