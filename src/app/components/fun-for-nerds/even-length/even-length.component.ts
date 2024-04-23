aba1612074463e554792ed4ebecc5c45a7129c1f
Anti Nou <anti_nou@hotmail.com>
2024-04-23 19:30:03 +0300

    fun for nerds

---
+++ src/app/components/fun-for-nerds/even-length/even-length.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-even-length',
+  standalone: true,
+  imports: [],
+  templateUrl: './even-length.component.html',
+  styleUrl: './even-length.component.css'
+})
+export class EvenLengthComponent {
+
+}
